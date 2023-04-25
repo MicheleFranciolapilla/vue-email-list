798cf47b9a3684b565768b973a278a6699a6ce55
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-04-25 16:16:45 +0900

    Completato, con bonus

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ createApp(
     },
     methods: 
     {
+        // Funzione "classica" che accumula il ritardo dovuto ai tempi di richiesta/risposta. N.B. Se si portassero gli output fuori dal ciclo e li si inserisse in un ciclo a se, successivo, avremmo solo output undefined (sempre a causa del ritardo temporale)
         get_data()
         {
             let list = document.getElementById("emails_list");
@@ -37,8 +38,31 @@ createApp(
                         console.log(i + 1, this.mails_array[i]);
                     });
             }
-            // this.mails_array = local_array;
-            // console.log(this.mails_array);
+            // Con cicli for separati: output undefined
+            // for (let i = 0; i < 10; i++)
+            // {
+            //     list.innerHTML += `<li> - ${this.mails_array[i]}</li>`;
+            //     console.log(i + 1, this.mails_array[i]);
+            // }
+        },
+
+        async async_get_data()
+        {
+            let list = document.getElementById("emails_list");
+            for (let i = 0; i < 10; i++)
+            {
+                await axios.get(this.boolean_API).then( result =>
+                    {
+                        this.mails_array.push(result.data.response);
+                        // list.innerHTML += `<li> - ${this.mails_array[i]}</li>`;
+                        // console.log(i + 1, this.mails_array[i]);
+                    });
+            }
+            for (let i = 0; i < 10; i++)
+            {
+                list.innerHTML += `<li> - ${this.mails_array[i]}</li>`;
+                console.log(i + 1, this.mails_array[i]);
+            }
         }
     }
 }).mount('#vue_app')
