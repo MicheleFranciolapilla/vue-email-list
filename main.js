f90f8f5ce343a828d62dd8691648049eb87b2fc7
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-04-25 11:28:00 +0900

    Metodo acquisizione + Metodo check ripetizione

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 // Script Vue 3
 const {createApp} = Vue 
 
@@ -6,6 +13,11 @@ createApp(
     data() 
     {
         return  {
+                    boolean_API:    "https://flynn.boolean.careers/exercises/api/random/mail",
+                    max_email_nr:   10,
+                    repeated:       0,
+                    counter:        0,
+                    emails_array:   []
                 }
     },
     created()
@@ -16,5 +28,20 @@ createApp(
     },
     methods: 
     {
+        get_data()
+        {
+            axios.get(boolean_API).then( result => {return result.data.response});
+        },
+
+        check_repeated(item)
+        {
+            let is_repeated = false;
+            if (this.emails_array.includes(item))
+            {
+                this.repeated++;
+                is_repeated = true;
+            }
+            return is_repeated;
+        }
     }
 }).mount('#vue_app')
